95d58b63e706de93f796823153de8c72ebbf9295
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-09-27 22:03:32 +0500

    init SpringBoot Project

---
+++ src/test/java/ru/usatu/students/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.usatu.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
